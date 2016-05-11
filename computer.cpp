d744d63b804ee7c21c6d01e06e66d14013c1132d
remid0 <remid.2002@gmail.com>
2016-05-11 11:30:59 +0200

    test2

---
+++ computer.cpp
@@ -0,0 +1,11 @@
+//
+//  computer.cpp
+//  Projet
+//
+//  Created by Rémi Di Vita on 26/04/2016.
+//  Copyright © 2016 Rémi Di Vita. All rights reserved.
+//
+
+#include "computer.hpp"
+
+//test1
---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Projet
+//
+//  Created by Rémi Di Vita on 26/04/2016.
+//  Copyright © 2016 Rémi Di Vita. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << atof("3.14")<<"\n";
+    return 0;
+}
